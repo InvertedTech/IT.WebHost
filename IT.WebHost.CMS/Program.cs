818ae8a07aa96ae584ea33750a7e5609002ab708
amingst <amingst97@gmail.com>
2026-04-09 13:08:59 -0400

    cms project

---
+++ IT.WebHost.CMS/Program.cs
@@ -0,0 +1,32 @@
+using BlazorBlueprint.Components;
+using IT.WebHost.CMS;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+// Add BlazorBlueprint services
+builder.Services.AddBlazorBlueprintComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(IT.WebHost.CMS.Client._Imports).Assembly);
+
+app.Run();
