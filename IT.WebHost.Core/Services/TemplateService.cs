2fd5a3bbfc4822dd4300971cf967de190f4c661a
Andrew Mingst <amingst97@gmail.com>
2026-04-10 19:18:03 -0400

    generate themes

--- IT.WebHost.Core/Services/TemplateService.cs
+++ IT.WebHost.Core/Services/TemplateService.cs
@@ -2,7 +2,26 @@ namespace IT.WebHost.Core.Services
 {
     public class TemplateService
     {
+        public readonly Dictionary<BaseFiles, string> BaseTemplates = new Dictionary<BaseFiles, string>()
+        {
+            { BaseFiles.Default, "default" },
+            { BaseFiles.Papyrus, "papyrus" },
+            { BaseFiles.Aurora, "aurora" },
+            { BaseFiles.Galaxy, "galaxy" },
+            { BaseFiles.Reef, "reef" },
+            { BaseFiles.Wildfire, "wildfire" }
+        };
         // todo: load this from settings service
-        public string TemplateFile { get; set; } = "default";
+        public string TemplateFile { get; set; } = "reef";
+    }
+
+    public enum BaseFiles
+    {
+        Default,
+        Papyrus,
+        Aurora,
+        Galaxy,
+        Wildfire,
+        Reef
     }
 }
