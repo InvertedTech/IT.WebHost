14dccca5431b4e72ce4a0e6e0f2875c99b4504d1
Phillip Fisher <phillip_fisher@hotmail.com>
2026-09-04 20:01:06 -0500

    working on sub stuff

--- App/App/Components/Pages/Subscribe/Subscribe.razor.cs
+++ App/App/Components/Pages/Subscribe/Subscribe.razor.cs
@@ -1,5 +1,7 @@
+using Antlr4.Runtime.Atn;
 using IT.Web.Project1.Services;
 using IT.WebHost.Core.Config;
+using IT.WebServices.Authentication;
 using IT.WebServices.Clients.Payment;
 using IT.WebServices.Fragments.Authorization;
 using IT.WebServices.Fragments.Authorization.Payment;
@@ -13,17 +15,26 @@ namespace WebApp.Components.Pages.Subscribe
         [Inject] NavigationManager Navigation { get; set; } = null!;
         [Inject] private SiteSettingsService SiteSettings { get; set; } = null!;
         [Inject] private PaymentClient PaymentClient { get; set; } = null!;
+        [Inject] private PaymentInterface.PaymentInterfaceClient RawPaymentClient { get; set; } = null!;
+        [Inject] private ONUserHelper UserHelper { get; set; } = null!;
         [Inject] private IOptions<AppSettings> _settings { get; set; } = null!;
 
-        private List<SubscriptionTier> tiers { get; set; } = new List<SubscriptionTier>();
+        private List<SubscriptionTier> tiers { get; set; } = [];
+        private GenericSubscriptionFullRecord[] mySubscriptions { get; set; } = [];
         private SubscriptionTier? selectedTier;
-        private string zipCode;
+        private string zipCode = "";
         private string selectedProvider = "Stripe";
         private bool isDialogOpen = false;
 
-        protected override void OnInitialized()
+        private GenericSubscriptionFullRecord[] ActiveSubscriptions => mySubscriptions.Where(s => s.SubscriptionRecord.Status == SubscriptionStatus.SubscriptionActive).ToArray();
+        private bool HasActiveSubscription => ActiveSubscriptions.Any();
+
+        protected override async Task OnInitializedAsync()
         {
             tiers = SiteSettings.Settings?.Subscription?.Tiers.ToList() ?? new();
+
+            var res = await RawPaymentClient.GetOwnSubscriptionRecordsAsync(new(), UserHelper.GetGrpcCallOptions());
+            mySubscriptions = [.. res.Generic];
         }
 
         public void OpenDialog(SubscriptionTier tier)
@@ -60,6 +71,11 @@ namespace WebApp.Components.Pages.Subscribe
             }
 
             // TODO: Display Error Messages
+        }
+
+        public async Task CancelSubscription(GenericSubscriptionFullRecord record)
+        {
+
         }
     }
 }
